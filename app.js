fe1b7bf44d5eb42e02c6c1fd79b93520f827f847
Hguillenalvarado <heinerguillen@gmail.com>
2021-03-13 13:54:28 +0000

    reorganizacion de for para el adj

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ var adj = ['great', 'big' ];
 var noun = ['jogger','racoon'];
 var domi=['.com','.net','.us','.io']
 
-
  function aleatorio(){
             let aleatoriodomi=Math.floor(Math.random()*domi.length);
             return domi[aleatoriodomi]
 }
-
-for(i=0;i<pronoun.length;i++){
-    for(j=0;j<adj.length;j++){
+for(j=0;j<adj.length;j++){
+    for(i=0;i<pronoun.length;i++){
+    
         for(k=0;k<noun.length;k++){
             console.log(pronoun[i] + adj[j] + noun[k] +aleatorio());
         }
