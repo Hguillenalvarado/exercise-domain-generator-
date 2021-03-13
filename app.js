6a486120c36122516f86b9502ed3043f9b5700b7
Hguillenalvarado <heinerguillen@gmail.com>
2021-03-13 13:28:00 +0000

    code arranque, pendiente detalles del for

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var pronoun = ['the','our'];
 var adj = ['great', 'big' ];
 var noun = ['jogger','racoon'];
+var domi=['.com','.net','.us','.io']
+
+
+ function aleatorio(){
+            let aleatoriodomi=Math.floor(Math.random()*domi.length);
+            return domi[aleatoriodomi]
+}
 
 for(i=0;i<pronoun.length;i++){
     for(j=0;j<adj.length;j++){
         for(k=0;k<noun.length;k++){
-            console.log(pronoun[i] +" "+ adj[j] +" "+ noun[k] +".com");
+            console.log(pronoun[i] + adj[j] + noun[k] +aleatorio());
         }
         
     }
